3adb9fc2ced47d873813ce9c24f1bd11e38d0f99
stinsonmathai <mathai.stinson@gmail.com>
2020-05-07 18:04:25 -0500

    updated

--- rf/get_model.py
+++ rf/get_model.py
@@ -8,6 +8,5 @@ def get_model(un, pw, url, api=1, unit=1):
     rfo = login(un, pw, url)
     res = rfo.get(f"/redfish/v{api}/Chassis/{unit}")
     # Add the error response if the response is not 200 OK
-    print(res)
     print(f"Model Description: {res.dict['Model']}")
     rfo.logout()
