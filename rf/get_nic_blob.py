903e8fec7875909d99890e9098a58685c1ad809a
stinsonmathai <mathai.stinson@gmail.com>
2020-05-14 08:06:27 -0500

    updates

--- rf/get_nic_blob.py
+++ rf/get_nic_blob.py
@@ -2,19 +2,18 @@ import json
 
 
 def get_nic_blob(rfo, api=1, unit=1):
-    """
-        This function fetches information about EACH of the network interfaces of the server.
-        URL is https://IP_ADDRESS/redfish/v1/systems/1/EthernetInterfaces/
+    """This function fetches information about EACH of the network interfaces of the server.
+    URL is https://IP_ADDRESS/redfish/v1/systems/1/EthernetInterfaces/
 
-        NOTE: There could be multiple interfaces in the server.
+    NOTE: There could be multiple interfaces in the server.
 
-            Parameters:
-            object: Redfish Client Login Object
-            int: API Value
-            int: Unit Value
+    Parameters:
+    object: Redfish Client Login Object
+    int: API Value
+    int: Unit Value
 
-            Returns:
-            JSON Array: Details of each of the Ethernet Interfaces in the Server
+    Returns:
+    JSON Array: Details of each of the Ethernet Interfaces in the Server
     """
     blob = []
     res = rfo.get(f"/redfish/v{api}/systems/{unit}/EthernetInterfaces")
@@ -26,9 +25,7 @@ def get_nic_blob(rfo, api=1, unit=1):
         res = rfo.get(m['@odata.id'])
         if res.status != 200:
             print(f"Member Error: {res.status}: {res.read}")
-            return "XXX"  #TODO should we break out here or just punch through the rest and note the error?
+            return "XXX"
         blob.append(res.dict)
-        #blob = blob + str(res.dict) #TODO -- this maynot be needed
-    blob_json = json.dumps(blob)
     # This will return a JSON Array which has all the values for each of the Interfaces, Best Viewed in a JSON Viewer
-    return blob_json
+    return json.dumps(blob)
