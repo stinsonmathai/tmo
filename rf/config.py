917f6d40771ef9b8a4a2cbab1cb974c23b86be18
John Richardson <jmrichardson@gmail.com>
2020-06-09 14:52:42 -0400

    Updates

--- rf/config.py
+++ rf/config.py
@@ -1,8 +1,21 @@
-un = "admin"
-pw = "admin"
-url = "https://16.91.158.182"
+# iLO 5 Tmo
+# un = "Administrator"
+# pw = "8M9Q8GDC"
+# url = "https://10.145.23.91"
+
+# iLO 4 Tmo
+# un = "Administrator"
+# pw = "59478916"
+# url = "https://10.145.143.7"
+
+# HPE New iLO5
+un = "Admin"
+pw = "Password"
+url = "https://16.83.131.155"
+
+api = 1
+unit = 1
 hpe_api = 1
 hpe_unit = 1
-
 dell_api = "System.Embedded.1"
 dell_unit = 1
